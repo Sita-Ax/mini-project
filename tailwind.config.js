87b9c4a10238081e2c56c035c55e65c5ca8c19d6
sita-Ax <sita81@hotmail.com>
2023-05-03 09:18:53 +0200

    darkmode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./dist*/index.html'],
+  darkMode: 'class', //media depend on your device mode or smartphone or class
   theme: {
     debugScreens: {
       position: ['top', 'left']
