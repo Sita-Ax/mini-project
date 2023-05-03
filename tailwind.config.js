efddf8422d5c240b2bb50658be027ad4bfb3ee07
sita-Ax <sita81@hotmail.com>
2023-05-03 11:56:01 +0200

    end of the first udemy mini

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./dist*/index.html'],
+  enabled: false, //have it on false when working on it
+  content: ['./dist/**/*.html'], //look for files inside of dist folder
   darkMode: 'class', //media depend on your device mode or smartphone or class
   theme: {
     debugScreens: {
